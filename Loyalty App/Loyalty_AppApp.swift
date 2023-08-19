9e98f7dc49c086ed75d085d86ad8943a6d47d8fb
Resn4R <purview-joust.0x@icloud.com>
2023-08-19 16:02:54 +0100

    Initial Commit

---
+++ Loyalty App/Loyalty_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loyalty_AppApp.swift
+//  Loyalty App
+//
+//  Created by Vito Borghi on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Loyalty_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
